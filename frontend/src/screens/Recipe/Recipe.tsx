ed4f21d0daccec045ac1958fb579c573edf24a64
computersaysnooo <geladg@gmail.com>
2025-09-18 15:54:56 +0300

    Feature: Add Timer component and integrate into Recipe screen

--- frontend/src/screens/Recipe/Recipe.tsx
+++ frontend/src/screens/Recipe/Recipe.tsx
@@ -1,10 +1,20 @@
 import "./Recipe.css";
+import Timer from "../../components/Timer/Timer";
+import useRecipe from "../../hooks/useRecipe";
+
 export default function Recipe() {
-	return (
-		<>
-			<div>
-				<h1>Recipe</h1>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <h1>Recipe</h1>
+        <h2>Preperation:</h2>
+        <useRecipe />
+        <div>Timer</div>
+        <Timer />
+        <h2>Cooking:</h2>
+        <h2>Baking:</h2>
+        <button>Finished, Go Back to Home Page</button>
+      </div>
+    </>
+  );
 }
