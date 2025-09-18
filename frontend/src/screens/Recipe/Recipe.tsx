2b0b8ebbbb2889030e637a7853af99f2136e2fcc
computersaysnooo <geladg@gmail.com>
2025-09-18 15:55:10 +0300

    Feature: Display recipe instructions and integrate Timer component in Recipe screen

--- frontend/src/screens/Recipe/Recipe.tsx
+++ frontend/src/screens/Recipe/Recipe.tsx
@@ -3,15 +3,17 @@ import Timer from "../../components/Timer/Timer";
 import useRecipe from "../../hooks/useRecipe";
 
 export default function Recipe() {
+  const { recipe } = useRecipe();
   return (
     <>
       <div>
         <h1>Recipe</h1>
         <h2>Preperation:</h2>
-        <useRecipe />
+
         <div>Timer</div>
         <Timer />
         <h2>Cooking:</h2>
+        <p>{recipe?.instructions}</p>
         <h2>Baking:</h2>
         <button>Finished, Go Back to Home Page</button>
       </div>
